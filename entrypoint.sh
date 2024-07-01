6fe9902747859a7605955575ba2bfede89396ae1
Liren <huanglr@cebitec.uni-bielefeld.de>
2024-07-01 14:07:21 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -309,7 +309,7 @@ do
     export GHRS_GITHUB_API_TOKEN="${INPUT_OWNTOKEN}"
 
     set +e
-    git push --set-upstream origin https://${INPUT_OWNTOKEN}@github.com/rhinempi/github-repo-stats.git
+    git push --set-upstream origin "${DATA_BRANCH_NAME}"
     PUSH_ECODE=$?
     set -e
     set +x
